30e73e3f32530ad05f0a88a3324fcca24f86f9de
Pankaj Barsaiya <pankajbars@gmail.com>
2025-04-25 07:50:16 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css"
 import Navbar from "./pages/Navbar/Navbar"
 import About from "./pages/About/About";
@@ -6,19 +7,29 @@ import Contact from "./pages/Contacts/Contact";
 import Skills from "./pages/Skills/Skills";
 import Projects from "./pages/Projects/Projects";
 import Education from "./pages/Education/Education";
+import { createContext } from "react";
 
+export const ThemeContext = createContext();
 function App(){
+
+  const [theme, setTheme] = useState("light");
+  
+  function toggleTheme(){
+    setTheme(theme === "light" ? "dark" : "light");
+  }
 
 
   return(
-    <>
+    <div className={theme}>
+    <ThemeContext value={{theme, toggleTheme}}>
     <Navbar/>
     <About />
     <Education />
     <Skills />
     <Projects />
     <Contact />
-    </>
+    </ThemeContext>
+    </div>
   )
 
 }
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import "./Navbar.css";
+import { useContext } from "react";
+import { ThemeContext } from "../../App";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(true);
 
+  const {theme, toggleTheme}=useContext(ThemeContext);
   return (
     <nav className="navbar">
       <a className="navbar-title" href="/">Portfolio</a>
@@ -18,6 +21,7 @@ function Navbar() {
         <li><a href="#skills" onClick={() => setMenuOpen(false)}>Skills</a></li>
         <li><a href="#projects" onClick={() => setMenuOpen(false)}>Projects</a></li>
         <li><a href="#contacts" onClick={() => setMenuOpen(false)}>Contacts</a></li>
+        <li><button onClick={toggleTheme} style={{fontWeight:"bold",fontSize:"20px" }}>{theme=="light"?"DARK":"LIGHT"} MODE</button></li>
 
       </ul>
     </nav>
